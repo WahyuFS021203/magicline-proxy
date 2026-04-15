b9be872dbf429af9b68930f2467798e37d0abeb0
WahyuFS021203 <ferziawans02@gmail.com>
2026-04-15 12:53:57 +0700

    modified:   api/get-customer.js

--- api/get-customer.js
+++ api/get-customer.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://your-webflow-domain.com",
+    "https://one-power-fitness.webflow.io/",
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
 
