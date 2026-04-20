46f3da311c35cf4ce2eedb4231a01bfa398d9583
WahyuFS021203 <ferziawans02@gmail.com>
2026-04-20 18:22:12 +0700

    update: fix update files

--- api/self-services/update/master-data.js
+++ api/self-services/update/master-data.js
@@ -10,7 +10,8 @@ export default async function handler(req, res) {
   );
 
   if (req.method === "OPTIONS") {
-    return res.status(200).end();
+    res.status(200).end();
+    return;
   }
 
   if (req.method !== "POST") {
